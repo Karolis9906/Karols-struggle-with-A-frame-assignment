815a64503057cc215996c750b380ecf8c9e5cd7f
Karolis9906 <58208586+Karolis9906@users.noreply.github.com>
2021-05-14 13:59:54 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,7 +5,7 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.x.rotation.y.rotation.z -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
